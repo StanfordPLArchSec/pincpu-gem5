StanfordPLArchSec/protean-gem5

eager/lazy/no branch handling

src/cpu/o3/BaseO3CPU.py
@@ -69,6 +69,8 @@ class DeclassifyMode(ScopedEnum):
 class TPTMode(ScopedEnum):
     vals = ["Naive", "Ideal", "YRoT", "None"]
 
+class ImplicitChannelMode(ScopedEnum):
+    vals = ["None", "Eager", "Lazy"]
 
 class BaseO3CPU(BaseCPU):
     type = "BaseO3CPU"
@@ -213,7 +215,7 @@ class BaseO3CPU(BaseCPU):
     # [TPT]
     tpt = Param.Bool(False, "Enable TPT")
     sttBugfixes = Param.Bool(True, "[STT] Apply bugfixes to restore security")
-    implicitChannel = Param.Bool(True, "If handling implicit channel")
+    implicitChannel = Param.ImplicitChannelMode("None", "[TPT] How to handle implicit channels")
     moreTransmitInsts = Param.Int(0, "More transmit instruction types")
     tptMode = Param.TPTMode("Naive", "TPT Mode")
     tptReg = Param.Bool(True, "[TPT] Enable r-taint primitives")

StanfordPLArchSec/pincpu-gem5

src/cpu/o3/BaseO3CPU.py
@@ -60,6 +60,11 @@ class CommitPolicy(ScopedEnum):
 class SpeculationModel(ScopedEnum):
     vals = ["None", "Ctrl", "CtrlSt", "Futuristic", "AtRet"]
 
+
+class ImplicitChannelMode(ScopedEnum):
+    vals = ["None", "Eager", "Lazy"]
+
+
 class BaseO3CPU(BaseCPU):
     type = "BaseO3CPU"
     cxx_class = "gem5::o3::CPU"
@@ -200,5 +205,5 @@ class BaseO3CPU(BaseCPU):
     # [STT] STT configurations
     stt = Param.Bool(False, "Apply STT protection mechanism")
     sttBugfixes = Param.Bool(False, "[STT] Apply bugfixes to restore security")
-    implicitChannel = Param.Bool(False, "If handling implicit channel")
+    implicitChannel = Param.ImplicitChannelMode("None", "[STT] How to handle implicit channels")
     moreTransmitInsts = Param.Int(0, "More transmit instruction types")