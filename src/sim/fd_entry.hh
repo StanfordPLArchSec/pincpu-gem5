2126c6dd55767c663be4f0ade169a4a881085464
root <root@pincpu-login3.c.soe-pincpu.internal>
2025-06-09 23:42:35 +0000

    update

--- src/sim/fd_entry.hh
+++ src/sim/fd_entry.hh
@@ -138,9 +138,8 @@ class FileFDEntry: public HBFDEntry
           _fileName(file_name), _fileOffset(file_offset)
     {
         _class = FDClass::fd_file;
-	char *path = realpath(_fileName.c_str(), nullptr);
-	panic_if(!path, "realpath failed!\n");
-        _fileName = path;
+	if (char *path = realpath(_fileName.c_str(), nullptr))
+	    _fileName = path;
 	std::free(path);
     }
 
